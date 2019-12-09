1bc9106765257601d2286c0f550b7ff1b18aa734
louwb <louwb@yunrong.cn>
2019-12-09 09:26:28 +0800

    feat:新增unsafe 的测试类

---
+++ web/src/test/java/com/david/example/juc/UnsafeTest.java
@@ -0,0 +1,43 @@
+package com.david.example.juc;
+
+import sun.misc.Unsafe;
+import sun.reflect.Reflection;
+
+import java.lang.reflect.Field;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @version $Id: null.java, v 1.0 2019/12/7 10:19 PM david Exp $$
+ * @Author:louwenbin(louwb@runyong.cn)
+ * @Description:Unsafe的 测试类
+ * @since 1.0
+ **/
+public class UnsafeTest {
+    public static void main(String[] args){
+
+        Integer a = Integer.valueOf(10);
+        // unsafe 的构造函数对classloader有限制，所以要用反射获取实例
+        Field unsafeObj = Unsafe.class.getDeclaredFields()[0];
+        unsafeObj.setAccessible(true);
+        Unsafe unsafe = null;
+        try {
+            unsafe = (Unsafe) unsafeObj.get(null);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        long offset = 0;
+        try {
+            offset =  unsafe.objectFieldOffset(Integer.class.getDeclaredField("value"));
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        //不知为啥，不起作用
+        unsafe.compareAndSwapInt(a,offset,2,10);
+        //可以正常使用
+        unsafe.putInt(a,offset,2);
+        System.out.println(a.intValue());
+
+    }
+}
